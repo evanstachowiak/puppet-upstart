fe0fe512f645817fb202e6ad2a941c4b4028a5e2
Evan Stachowiak <evan.stachowiak@gmail.com>
2012-09-25 15:49:23 -0400

    separate define from init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,7 @@
 class upstart {
-    package { upstart:
-        ensure => installed,
-    }
-}
 
-define upstart::job($description = '', $fork=false, $cmd='', $use_script=false) {
-    file { "/etc/init/${title}.conf":
-        content => template('upstart/job.conf.erb'),
-        ensure => present
-    }
+  package { 'upstart':
+    ensure => installed,
+  }
+
 }
