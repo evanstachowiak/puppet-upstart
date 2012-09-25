eb7706f4e5bc75cbc136c120a952679ff9a5f66b
Evan Stachowiak <evan.stachowiak@gmail.com>
2012-09-25 16:18:31 -0400

    add params.pp file

---
+++ manifests/params.pp
@@ -0,0 +1,21 @@
+# Class upstart::params
+#
+# All default parameters for upstart class
+#
+# == Parameters
+#
+#
+# == Examples
+#
+#
+# == Requires:
+#
+# This class file is not called directly
+class upstart::params {
+
+  $cmd         = ''
+  $description = ''
+  $expect      = 'fork'
+  $use_script  = false
+
+}
